b088a40a03a6deda8086f68e663109d450375e50
JavaScriptN0ob <wenpeijs@gmail.com>
2020-07-24 14:33:32 +1000

    minor changes

--- getWeather.js
+++ getWeather.js
@@ -1,46 +1,36 @@
+const { response } = require('express');
+
 function getWeather(city) {
-  var https = require('follow-redirects').https;
-  var fs = require('fs');
+  var axios = require('axios');
   var cityName = city
-  var options = {
-    'method': 'GET',
-    'hostname': 'api.openweathermap.org',
-    'path': `/data/2.5/weather?q=${city}%2Cau&appid=864eadc0aaaab505b46c26f231ae8840&units=metric`,
-    'headers': {
-    },
-    'maxRedirects': 20
+  var cityWeather = {
+    temp: null,
+    humidity: null,
+    wind: null,
+  }
+  var config = {
+    method: 'get',
+    url: `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=864eadc0aaaab505b46c26f231ae8840&units=metric`,
+    headers: { }
   };
-
-  var req = https.request(options, function (res) {
-    var chunks = [];
-
-    res.on("data", function (chunk) {
-      chunks.push(chunk);
-    });
-
-    res.on("end", function (chunk) {
-      var body = Buffer.concat(chunks);
-      let weather = JSON.parse(body);
-      // return {
-      //   "temp": `${weather.main.temp.toFixed(1)}℃`,
-      //   "wind-speed": `${weather.wind.speed}M/S`,
-      //   "humidity": `${weather.main.humidity}%`
-      // }
-      console.log('temp:', `${weather.main.temp.toFixed(1)}℃`);
-      console.log('wind-speed', `${weather.wind.speed}M/S`);
-      console.log('humidity', `${weather.main.humidity}%`)
-      return weather;
-    });
-
-    res.on("error", function (error) {
-      console.error(error);
-    });
+  axios(config)
+  .then(function (response) {
+    cityWeather.temp = `${response.data.main.temp}℃`;
+    cityWeather.humidity = `${response.data.main.humidity}%`;
+    cityWeather.wind = `${response.data.wind.speed}M/S`;
+    console.log(cityWeather)
+  })
+  .catch(function (error) {
+    console.log(error);
   });
 
-  req.end();
+  console.log(cityWeather);
 }
 
 getWeather('Sydney');
-console.log(typeof(getWeather('Sydney')));
+console.log(getWeather('Sydney'))
+console.log(123, typeof(getWeather('Sydney')));
 
 module.exports = { getWeather };
+
+
